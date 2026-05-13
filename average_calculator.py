f0696e259bc23f297492febad7a516dcd4970bd7
IsmaGarciaC <melgarciacas@gmail.com>
2026-05-13 17:37:27 -0500

    calculate_average and determine_status functions added

--- average_calculator.py
+++ average_calculator.py
@@ -9,9 +9,11 @@ def enter_grades():
         subject = input("Enter Subject: ")
         grade = float(input("Enter a new grade between 0 and 10: "))
 
-        while grade < 0 or grade > 10:
+        while grade < 0 or grade > 10: #Checking if the grade is valid
             print("Error: The grade must be between 0 and 10.")
             grade = float(input("Try again. Enter grade: "))
+        
+        #Add the values to the list
         subjects.append(subject)
         grades.append(grade)
 
@@ -20,4 +22,19 @@ def enter_grades():
             break
 
     return subjects, grades
+
+def calculate_average(grades):
+    average_grade = sum(grades) / len(grades) # The formula
+    return average_grade
+
+def determine_status(grades, threshold = 5.0):
+    passed = []
+    failed = []
+
+    for index, grade in enumerate(grades):
+        if grade < threshold:
+            failed.append(index)
+        else:
+            passed.append(index)
+    return passed, failed
 
